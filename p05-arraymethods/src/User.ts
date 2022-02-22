53b4ac0bf5cf3da554ea43bac89b2197d4593e8b
Muraoka <muraoka@villhill.net>
2022-02-22 15:29:25 +0900

    added User.class and createRandomUser method

---
+++ p05-arraymethods/src/User.ts
@@ -0,0 +1,30 @@
+export class User {
+
+  public constructor(private _name: string, private _money : number) {
+    this._name = _name;
+    this._money = _money;
+  }
+
+  // 所持金を2倍にするメソッド
+  public double() {
+    this._money *= 2;
+  }
+
+  // Getters
+  get name() {
+    return this._name;
+  }
+
+  get money() {
+    return this._money;
+  }
+
+}
+
+export async function createRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+  const name = data.results[0];
+  const money =  Math.floor(Math.random() * 1000000);
+  return new User(name, money);
+}
